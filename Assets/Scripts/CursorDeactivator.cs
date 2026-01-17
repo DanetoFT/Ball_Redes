947d552244ef8dec06af715fd5d19ce6844c3f6f
Dani <danielferrert10@gmail.com>
2026-01-17 22:43:16 +0100

    FINAL DE VERDAD

--- Assets/Scripts/CursorDeactivator.cs
+++ Assets/Scripts/CursorDeactivator.cs
@@ -1,12 +1,20 @@
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CursorDeactivator : MonoBehaviour
 {
+    public Button loadGameButton;
+    public Button loadGameButton2;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+
+        loadGameButton.interactable = NetworkManager.Singleton.IsHost;
+        loadGameButton2.interactable = NetworkManager.Singleton.IsHost;
     }
 
 }
